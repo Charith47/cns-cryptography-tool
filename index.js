73d29097467846989293cbeb51edd06e374bb3a8
Charith Dissanayake <charithdissanayake47@gmail.com>
2022-03-14 10:45:34 +0530

    finished enc/dec functions

--- index.js
+++ index.js
@@ -17,78 +17,71 @@ submitButton.addEventListener('click', async function (e) {
 	if (plaintext.trim() === '') return alert('An input text is required');
 	if (password.trim() === '') return alert('A password is required');
 
-	console.log(plaintext, password);
+	ciphertext = await encrypt(crypto, plaintext, password);
+});
 
+decryptButton.addEventListener('click', async function (e) {
+	e.preventDefault();
+
+	const _oPassword = document.getElementById('user-key-output');
+	const _oPlaintext = document.getElementById('plaintext-output');
+	const oPassword = _oPassword.value;
+
+	if (oPassword.trim() === '') return alert('A password is required');
+
+	try {
+		const decryptedText = await decrypt(crypto, ciphertext, oPassword);
+		_oPlaintext.innerText = decryptedText;
+	} catch (error) {
+		alert(`Decryption failed: ${error}`);
+	}
+});
+
+const encrypt = async function encrypt(crypto, plaintext, password) {
 	const pswUtf8 = new TextEncoder().encode(password);
 	const pswHash = await crypto.subtle.digest('SHA-256', pswUtf8);
-
 	const iv = crypto.getRandomValues(new Uint8Array(12));
 	const ivString = Array.from(iv)
-		.map((bytes) => String.fromCharCode(bytes))
+		.map((byte) => String.fromCharCode(byte))
 		.join('');
-
-	const options = {
-		name: 'AES-GCM',
-		iv: iv,
-	};
-
+	const options = { name: 'AES-GCM', iv: iv };
 	const key = await crypto.subtle.importKey('raw', pswHash, options, false, [
 		'encrypt',
 	]);
-
-	const plainTextUint8 = new TextEncoder().encode(plaintext);
+	const plaintextUint8 = new TextEncoder().encode(plaintext);
 	const characterBuffer = await crypto.subtle.encrypt(
 		options,
 		key,
-		plainTextUint8
+		plaintextUint8
 	);
-
 	const characterArray = Array.from(new Uint8Array(characterBuffer));
 	const characterString = characterArray
 		.map((byte) => String.fromCharCode(byte))
 		.join('');
+	return btoa(ivString + characterString);
+};
 
-	ciphertext = btoa(characterString);
-	console.log(btoa(characterString));
-});
-
-decryptButton.addEventListener('click', async function () {
-	const pswUtf8 = new TextEncoder().encode(password); // todo get psw input
+const decrypt = async function decrypt(crypto, ciphertext, password) {
+	const pswUtf8 = new TextEncoder().encode(password);
 	const pswHash = await crypto.subtle.digest('SHA-256', pswUtf8);
-
 	const ivString = atob(ciphertext).slice(0, 12);
-	const iv = new Uint8Array(
-		Array.from(ivString).map((char) => {
-			char.charCodeAt(0);
-		})
-	);
-
-	const options = {
-		name: 'AES-GCM',
-		iv: iv,
-	};
-
+	const iv = new Uint8Array(Array.from(ivString).map((ch) => ch.charCodeAt(0)));
+	const options = { name: 'AES-GCM', iv: iv };
 	const key = await crypto.subtle.importKey('raw', pswHash, options, false, [
 		'decrypt',
 	]);
-
 	const characterString = atob(ciphertext).slice(12);
 	const characterUint8 = new Uint8Array(
-		Array.from(characterString).map((character) => character.charCodeAt(0))
+		Array.from(characterString).map((ch) => ch.charCodeAt(0))
 	);
-
 	try {
 		const plainBuffer = await crypto.subtle.decrypt(
 			options,
 			key,
 			characterUint8
 		);
-		const plaintext = new TextDecoder().decode(plainBuffer);
-
-		console.log(plaintext);
+		return new TextDecoder().decode(plainBuffer);
 	} catch (error) {
-		console.log(error);
+		throw new Error(error.message);
 	}
-});
-
-//const crypto = self.crypto
+};
